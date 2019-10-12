3c5abe94bb6c2e8fe84bf29e580403ff1bceded3
Hannah He <hannahhe@Hannahs-MBP.wv.cc.cmu.edu>
2019-10-12 01:05:55 -0400

    Added detailed comments for createRandomHotspotEnv.m

--- createRandomHotspotEnv.m
+++ createRandomHotspotEnv.m
@@ -1,8 +1,10 @@
 function [x,y,z] = createRandomHotspotEnv(numHotSpots,rangeX,rangeY,numX,numY)
-x = linspace(rangeX(1),rangeX(2),numX);
-y = linspace(rangeY(1),rangeY(2),numY);
-[x,y] = meshgrid(x,y);
-z = zeros(size(x));
+%create base environment
+x = linspace(rangeX(1),rangeX(2),numX); %create a x vector with a specified length
+y = linspace(rangeY(1),rangeY(2),numY); %create a y vector with a specified length
+[x,y] = meshgrid(x,y); %fill in the rest of the grid from the x,y axis
+
+z = zeros(size(x)); %create a matrix for hotspots
 for i = 1:numHotSpots
     hotspotCenter = [rangeX(1)+rand*(rangeX(2)-rangeX(1)),rangeY(1)+rand*(rangeY(2)-rangeY(1))];
     hotspotR1 = min(range(rangeX),range(rangeY))*0.1*rand;
